9e7e6f95c58ac25a9274d3f8b8e7dd4771a8bb3f
jlucas86 <joshua.lucas014@gmail.com>
2024-12-18 03:09:25 -0500

    added function to SecurityService to check if the loged in user has a role. has not been tested yet

--- src/main/java/com/example/userInfo/authentication/SecurityService.java
+++ src/main/java/com/example/userInfo/authentication/SecurityService.java
@@ -2,6 +2,8 @@ package com.example.userInfo.authentication;
 
 
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.example.role.Role;
+import com.example.role.RoleService;
+import com.example.userInfo.UserInfo;
 import com.example.userInfo.UserInfoService;
 
 @Service("securityService")
@@ -16,12 +21,26 @@ public class SecurityService {
     
     @Autowired
     UserInfoService userInfoService;
+    @Autowired
+    RoleService roleService;
     Logger logger = LoggerFactory.getLogger(SecurityService.class);
     Authentication authentication;
 
     public boolean isUser(String username){
         this.authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName().equals(username); // || user has role of adin;
+    }
+
+    public boolean hasAuth(){
+
+        Optional<Role> r = roleService.getRole(1);
+        if(!r.isPresent()){
+            // throw error for role not found
+            return false;
+        }
+        this.authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserInfo u = (UserInfo) authentication.getPrincipal();
+        return u.getRoles().contains(r.get());
     }
 
 }
