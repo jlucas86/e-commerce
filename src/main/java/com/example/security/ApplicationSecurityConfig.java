03a396d118bd679b4feaeca0398b68aadc0833e9
jlucas86 <joshua.lucas014@gmail.com>
2024-12-20 03:27:10 -0500

    removed redirect for logout, and some format changes

--- src/main/java/com/example/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/ApplicationSecurityConfig.java
@@ -1,66 +1,34 @@
 package com.example.security;
 
-import org.hibernate.dialect.PostgreSQLDriverKind;
+import java.io.IOException;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.example.auth.ApplicationUser;
-// import com.example.auth.ApplicationUser;
 import com.example.auth.ApplicationUserService;
-import com.example.permission.Permission;
-import com.example.role.Role;
-import com.example.userInfo.UserInfo;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import static com.example.security.ApplicationUserRole.*;
-import static com.example.security.ApplicationUserPermission.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Supplier;
-
-import javax.sql.DataSource;
 
 // import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 
@@ -148,7 +116,7 @@ public class ApplicationSecurityConfig {
                                                                                                                     */)
                                 .logout((logout) -> logout.logoutUrl("/logout").clearAuthentication(true)
                                                 .invalidateHttpSession(true).deleteCookies("remember-me", "JSESSIONID")
-                                                .logoutSuccessUrl("http://localhost:3000/"));
+                                                );
 
                 // .authorizeHttpRequests((authz)->{
                 // authz
